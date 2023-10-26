a5d2db85a1a1a3d6a6a0f8c76d69db4dc98a22c0
SiddhantGC <138275731+SiddhantGC@users.noreply.github.com>
2023-10-26 10:04:12 +0530

    Update cv.py

--- cv.py
+++ cv.py
@@ -1,1 +1,13 @@
-import cv2
+import streamlit as st
+
+# Streamlit app title and description
+st.title("First Name Printer")
+st.write("Enter your first name and see it printed below.")
+
+# Input for the first name
+first_name = st.text_input("Enter your first name")
+
+# Check if the input is not empty and display it
+if first_name:
+    st.write(f"Hello, {first_name}!")
+
